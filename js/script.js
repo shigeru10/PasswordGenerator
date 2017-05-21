dc55a7bb707a8914929d0de6f2ca7c2f0dc6a3fd
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-05-21 12:10:33 +0900

    get Password

--- js/script.js
+++ js/script.js
@@ -2,7 +2,23 @@
 
 var slider = document.getElementById('slider');
 var label = document.getElementById('label');
+var btn = document.getElementById('btn');
+var result = document.getElementById('result');
+
+function getPassword() {
+  result.value = 'aaaaaaa';
+}
 
 slider.addEventListener('change', function() {
   label.innerHTML = this.value;
 });
+
+btn.addEventListener('click', function() {
+  getPassword();
+});
+
+result.addEventListener('click', function() {
+  this.select();
+});
+
+getPassword();
