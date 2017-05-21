79700c2cbc47bda143b2112d4227cc8d45401798
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-05-21 12:03:29 +0900

    slider motion

---
+++ js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var slider = document.getElementById('slider');
+var label = document.getElementById('label');
+
+slider.addEventListener('change', function() {
+  label.innerHTML = this.value;
+});
